1290707a23807342946fd61b4d12c50debafc72b
Gionata Grotto <g.gionata99@gmail.com>
2026-04-06 21:53:37 +0200

    more ICP implementation

--- src/Odometry.cpp
+++ src/Odometry.cpp
@@ -1,6 +1,7 @@
 #include "Odometry.h"
 #include <glm/gtc/matrix_transform.hpp>
 #include <limits>
+#include <Eigen/Dense>
 
 
 glm::mat4 Odometry::computeICP(const std::vector<glm::vec3>& source, 
@@ -41,17 +42,41 @@ glm::mat4 Odometry::computeICP(const std::vector<glm::vec3>& source,
         glm::vec3 center_source = getCenterOfMass(source_matched);
         glm::vec3 center_target = getCenterOfMass(target_matched);
 
-        glm::vec3 t = center_target - center_source;
+        Eigen::Matrix3f H = Eigen::Matrix3f::Zero();
         
-        glm::mat4 T_step = glm::translate(glm::mat4(1.0f), t);
+        for (size_t i = 0; i < source_matched.size(); ++i) {
+            Eigen::Vector3f s(source_matched[i].x - center_source.x, source_matched[i].y - center_source.y, source_matched[i].z - center_source.z);
+            Eigen::Vector3f t(target_matched[i].x - center_target.x, target_matched[i].y - center_target.y, target_matched[i].z - center_target.z);
+            
+            H += s * t.transpose();
+        }
+
+        Eigen::JacobiSVD<Eigen::Matrix3f> svd(H, Eigen::ComputeFullU | Eigen::ComputeThinV);
+        Eigen::Matrix3f U = svd.matrixU();
+        Eigen::Matrix3f V = svd.matrixV();
+        Eigen::Matrix3f R_eigen = V * U.transpose();
+
+        if (R_eigen.determinant() < 0) {
+            V.col(2) *= -1;
+            R_eigen = V * U.transpose();
+        }
+
+        glm::mat4 R_glm =  glm::mat4(1.0f);
+        for (size_t x = 0; x < 3; ++x) {
+            for (size_t y = 0; y < 3; ++y) {
+                R_glm[x][y] = R_eigen(y, x);
+            }
+        }
+
+        glm::vec3 t_final = center_target - glm::vec3(R_glm * glm::vec4(center_source, 1.0f));
+
+        glm::mat4 T_step = glm::translate(glm::mat4(1.0f), t_final) * R_glm;
 
         for (auto& p : source_curr) {
-            p = glm::vec3(T_step * glm::vec4(p, 1.0f));
+            p = glm::vec4(T_step * glm::vec4(p,1.0f));
         }
-        T_total = T_step * T_total;       
         
     }
-
     return T_total;
 }
 
